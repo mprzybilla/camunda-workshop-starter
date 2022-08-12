dfa59337294079f9b5b1b56129a7ff560e21e03b
mprzybilla <marcus@przybilla.email>
2022-08-12 11:03:02 +0200

    Basis-Starter - initial commit

---
+++ src/main/java/de/bag/camundaws/CamundaWorkshopApplication.java
@@ -0,0 +1,11 @@
+package de.bag.camundaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaWorkshopApplication {
+  public static void main(String... args) {
+    SpringApplication.run(CamundaWorkshopApplication.class, args);
+  }
+}
